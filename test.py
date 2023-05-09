a6c0f6b266202b5a4fd3b2fafe270dafaa3ac0c5
Mohammadjavad Salehi <83529512+MohammadjavadSalehi@users.noreply.github.com>
2023-05-09 03:55:52 -0700

    Update test.py

--- test.py
+++ test.py
@@ -3,39 +3,41 @@ from io import StringIO
 from unittest.mock import patch
 
 class TestCombineNames(unittest.TestCase):
-    
-    @patch('builtins.input', side_effect=["John", "Doe"])
+    @patch("builtins.input", side_effect=["John", "Doe"])
     def test_combine_names(self, mock_input):
         expected_output = "John Doe"
-        with patch('sys.stdout', new=StringIO()) as fake_output:
+        with patch("sys.stdout", new=StringIO()) as fake_output:
             full_name = combine_names()
             self.assertEqual(full_name, expected_output)
             self.assertEqual(fake_output.getvalue().strip(), expected_output)
-            
-    @patch('builtins.input', side_effect=["Jane", "Doe"])
+
+
+    @patch("builtins.input", side_effect=["Jane", "Doe"])
     def test_combine_names_custom_input(self, mock_input):
         expected_output = "Jane Doe"
-        with patch('sys.stdout', new=StringIO()) as fake_output:
+        with patch("sys.stdout", new=StringIO()) as fake_output:
             full_name = combine_names()
             self.assertEqual(full_name, expected_output)
             self.assertEqual(fake_output.getvalue().strip(), expected_output)
-            
-    @patch('builtins.input', side_effect=["", ""])
+
+
+    @patch("builtins.input", side_effect=["", ""])
     def test_combine_names_empty_input(self, mock_input):
         expected_output = " "
-        with patch('sys.stdout', new=StringIO()) as fake_output:
+        with patch("sys.stdout", new=StringIO()) as fake_output:
             full_name = combine_names()
             self.assertEqual(full_name, expected_output)
             self.assertEqual(fake_output.getvalue().strip(), expected_output)
-    
-    @patch('builtins.input', side_effect=["John", ""])
+
+
+    @patch("builtins.input", side_effect=["John", ""])
     def test_combine_names_missing_last_name(self, mock_input):
         expected_output = "John "
-        with patch('sys.stdout', new=StringIO()) as fake_output:
+        with patch("sys.stdout", new=StringIO()) as fake_output:
             full_name = combine_names()
             self.assertEqual(full_name, expected_output)
             self.assertEqual(fake_output.getvalue().strip(), expected_output)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
