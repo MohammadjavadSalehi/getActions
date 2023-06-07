cab4e7db8163fc044db7f0f23df0e653cff68b2d
Mohammadjavad Salehi <83529512+MohammadjavadSalehi@users.noreply.github.com>
2023-06-07 12:30:00 -0700

    fix bugs

--- test.py
+++ test.py
@@ -9,33 +9,36 @@ from rps_game import play_game, run_game
 class TestRPSGame(unittest.TestCase):
 
     # Test the play_game() function
-    def test_play_game_tie(self):
+    @patch('builtins.input', return_value='rock')
+    def test_play_game_tie(self, mock_input):
         with patch('sys.stdout', new=StringIO()) as fake_output:
             random.seed(1)
             play_game()
             self.assertEqual(fake_output.getvalue().strip(), "It's a tie!")
 
-    def test_play_game_win(self):
+    @patch('builtins.input', return_value='paper')
+    def test_play_game_win(self, mock_input):
         with patch('sys.stdout', new=StringIO()) as fake_output:
             random.seed(2)
             play_game()
             self.assertEqual(fake_output.getvalue().strip(), "You win!")
 
-    def test_play_game_loss(self):
+    @patch('builtins.input', return_value='scissors')
+    def test_play_game_loss(self, mock_input):
         with patch('sys.stdout', new=StringIO()) as fake_output:
             random.seed(3)
             play_game()
             self.assertEqual(fake_output.getvalue().strip(), "You lose!")
 
     # Test the run_game() function
-    def test_run_game_once(self):
+    @patch('builtins.input', side_effect=['rock', 'no'])
+    def test_run_game_once(self, mock_input):
         with patch('sys.stdout', new=StringIO()) as fake_output:
             random.seed(4)
-            with patch('builtins.input', return_value='rock'):
-                run_game()
-                self.assertIn("You chose rock, and the computer chose paper.", fake_output.getvalue())
-                self.assertIn("You lose!", fake_output.getvalue())
-                self.assertIn("Do you want to play again? (yes/no): ", fake_output.getvalue())
+            run_game()
+            self.assertIn("You chose rock, and the computer chose paper.", fake_output.getvalue())
+            self.assertIn("You lose!", fake_output.getvalue())
+            self.assertIn("Do you want to play again? (yes/no): ", fake_output.getvalue())
 
     @patch('builtins.input', side_effect=['yes', 'rock', 'yes', 'paper', 'no'])
     def test_run_game_twice(self, mock_input):
@@ -72,7 +75,6 @@ class TestRPSGame(unittest.TestCase):
             self.assertIn("You chose rock, and the computer chose scissors.", fake_output.getvalue())
             self.assertIn("You win!", fake_output.getvalue())
             self.assertIn("Do you want to play again? (yes/no): ", fake_output.getvalue())
-
 
 if __name__ == '__main__':
     unittest.main()
